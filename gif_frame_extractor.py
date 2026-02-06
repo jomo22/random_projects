00087c61f8bcf97dca04906cf70fc2389c4fb7bf
Josiah M <143579471+jomo22@users.noreply.github.com>
2026-02-06 00:15:12 -0600

    Update gif_frame_extractor.py
    Updated the directory path for saving frames to a relative path.

--- gif_frame_extractor.py
+++ gif_frame_extractor.py
@@ -5,7 +5,8 @@ from PIL import Image, ImageSequence
 gif = Image.open("happy-kid.gif")
 # saving each frame as a separate image
 
-os.chdir(r"C:\Users\Josiah\Desktop\School\coding projects\python gif\source images")
+os.chdir(r"source images") # change to your source image directory
 
 for i, frame in enumerate(ImageSequence.Iterator(gif)):
     frame.save(f"frame_{i:02d}.png")
+
